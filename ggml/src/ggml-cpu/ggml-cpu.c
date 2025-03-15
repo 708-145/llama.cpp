d4e225937a51de62c03e30a593d7b6fd86fff822
708-145 <tobii@gmx.de>
2025-03-16 00:57:09 +0100

    debugging

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -7528,78 +7528,6 @@ static void ggml_compute_forward_mul_mat_one_chunk(
     }
 }
 
-static void ggml_compute_forward_mul_mat_one_chunk_IQ4_NL_F32( // TB:
-    const struct ggml_compute_params * params,
-    struct ggml_tensor * dst,
-    const enum ggml_type type,
-    const int64_t num_rows_per_vec_dot,
-    const int64_t ir0_start,
-    const int64_t ir0_end,
-    const int64_t ir1_start,
-    const int64_t ir1_end) {
-
-    const struct ggml_tensor * src0 = dst->src[0];
-    const struct ggml_tensor * src1 = dst->src[1];
-
-    GGML_TENSOR_BINARY_OP_LOCALS
-
-    assert(ggml_is_contiguous(src1) == 1); // if this ever hits, add condition into type switch fo IQ4_NL bpp handling
-
-    ggml_vec_dot_t const vec_dot      = type_traits_cpu[type].vec_dot;
-    enum ggml_type const vec_dot_type = type_traits_cpu[type].vec_dot_type;
-
-    // broadcast factors
-    const int64_t r2 = ne12 / ne02;
-    const int64_t r3 = ne13 / ne03;
-
-    //printf("ir0_start = %6lld, ir0_end = %6lld, ir1_start = %6lld, ir1_end = %6lld\n", ir0_start, ir0_end, ir1_start, ir1_end);
-	// ir0_start =   1984, ir0_end =   2048, ir1_start =      0, ir1_end =      1
-
-    // threads with no work simply yield (not sure if it helps)
-    if (ir0_start >= ir0_end || ir1_start >= ir1_end) {
-        return;
-    }
-
-	const void * wdata = (src1->type == vec_dot_type) ? src1->data : params->wdata; // TB: modify for F32 handling
-	const size_t row_size = ggml_row_size(vec_dot_type, ne10);
-
-	assert(ne12 % ne02 == 0);
-	assert(ne13 % ne03 == 0);
-
-	const size_t src1_col_stride = row_size; // given that src1_cont is true
-	//printf("src1_col_stride = %6lld\n", src1_col_stride);
-
-	for (int64_t ir1 = ir1_start; ir1 < ir1_end; ir1 += num_rows_per_vec_dot) {
-		const int64_t i13 = (ir1 / (ne12 * ne1));
-		const int64_t i12 = (ir1 - i13 * ne12 * ne1) / ne1;
-		const int64_t i11 = (ir1 - i13 * ne12 * ne1 - i12 * ne1);
-
-		// broadcast src0 into src1
-		const int64_t i03 = i13 / r3;
-		const int64_t i02 = i12 / r2;
-
-		const int64_t i1 = i11;
-		const int64_t i2 = i12;
-		const int64_t i3 = i13;
-
-		const char * src0_row = (const char*)src0->data + (0 + i02 * nb02 + i03 * nb03);
-
-		// we know that src1_cont is true
-		const char * src1_col = (const char*)wdata + (i11 + i12 * ne11 + i13 * ne12 * ne11) * row_size;
-		float * dst_col = (float*)((char*)dst->data + (i1 * nb1 + i2 * nb2 + i3 * nb3));
-
-		for (int64_t ir0 = ir0_start; ir0 < ir0_end; ir0 += num_rows_per_vec_dot) {
-		    float tmp;
-		    vec_dot(ne00, &tmp, 0, src0_row + ir0 * nb01, (num_rows_per_vec_dot > 1 ? nb01 : 0), src1_col, (num_rows_per_vec_dot > 1 ? src1_col_stride : 0), num_rows_per_vec_dot);
-
-			for (int cn = 0; cn < num_rows_per_vec_dot; ++cn) {
-				memcpy(&dst_col[ir0 + cn * nb1 / nb0], &tmp, sizeof(float));
-			}
-		}
-	}
-
-} // ggml_compute_forward_mul_mat_one_chunk_IQ4_NL_F32
-
 static void ggml_compute_forward_mul_mat_orig(
         const struct ggml_compute_params * params,
               struct ggml_tensor * dst) {
@@ -7813,130 +7741,19 @@ static void ggml_compute_forward_mul_mat_iq4_nl( // TB: IQ4_NL variant
     const int nth = params->nth;
 
 	// print dimension variables:
-	printf("dimension: rows %ld, cols %ld, Byte per row %ld\n", ne0, nb01*16/9, nb01);
+	//printf("dimension: rows %ld, cols %ld, Byte per row %ld\n", ne0, nb01*16/9, nb01);
 	// ne0  = IQ4NL rows == result vector length
 	// nb01*8/4.5 = IQ4NL columns
 	// nb01 = Bytes per IQ4NL row (4.5bpw, 864 for 1536, 2304 for 4096)
 	
 	// assign chunks to thread, split by output vector only despite being more LUT effort
+	const int64_t vecsize = ne0;
 	const int64_t num_rows_per_chunk = ne0 / nth;
 	const int64_t from_row = ith * num_rows_per_chunk;
 	const int64_t to_row   = MIN(from_row+num_rows_per_chunk,ne0)-1;
-	printf("thread %d computes row %ld to %ld; %ld of %ld\n", ith, from_row, to_row, to_row-from_row+1, num_rows_per_chunk);
-	//bpp_IQ4NL_F32_vecmul(params, dst, from_row, to_row);
-	dummyPrint();
-
-
-	// old code starts here:
-    enum ggml_type           const vec_dot_type         = type_traits_cpu[src0->type].vec_dot_type;
-    ggml_from_float_t        const from_float           = type_traits_cpu[vec_dot_type].from_float;
-    //int64_t                  const vec_dot_num_rows     = type_traits_cpu[src0->type].nrows;
-	
-    if (ith == 0) {
-		//printf("ggml_compute_forward_mul_mat_iq4_nl type %s, %s\n", ggml_type_name(src0->type), ggml_type_name(src1->type));
-		//dummyPrint();
-	}
-
-	// TB: cope with src1-> type F32 below, skip here 
-    if (src1->type != vec_dot_type) { 
-        char * wdata = params->wdata;
-
-        const size_t nbw0 = ggml_type_size(vec_dot_type);
-        const size_t nbw1 = ggml_row_size(vec_dot_type, ne10);
-        const size_t nbw2 = nbw1*ne11;
-        const size_t nbw3 = nbw2*ne12;
-
-		//printf("ggml_compute_forward_mul_mat_iq4_nl type %s, %s\n", ggml_type_name(src1->type), ggml_type_name(vec_dot_type));
-		
-        assert(params->wsize >= ne13*nbw3);
-        GGML_ASSERT(src1->type == GGML_TYPE_F32);
-
-    #if 0
-        for (int64_t i13 = 0; i13 < ne13; ++i13) {
-            for (int64_t i12 = 0; i12 < ne12; ++i12) {
-                for (int64_t i11 = ith; i11 < ne11; i11 += nth) {
-                    from_float((float *)((char *) src1->data + i13*nb13 + i12*nb12 + i11*nb11),
-                               (void *)               (wdata + i13*nbw3 + i12*nbw2 + i11*nbw1),
-                                ne10);
-                }
-            }
-        }
-    #else
-        for (int64_t i13 = 0; i13 < ne13; ++i13) {
-            for (int64_t i12 = 0; i12 < ne12; ++i12) {
-                for (int64_t i11 = 0; i11 < ne11; ++i11) {
-                    size_t bs = ggml_blck_size(vec_dot_type);
-                    int64_t ne10_block_start = (ith * ne10/bs) / nth;
-                    int64_t ne10_block_end   = ((ith + 1) * ne10/bs) / nth;
-                    from_float((float *)((char *) src1->data + i13*nb13 + i12*nb12 + i11*nb11 + ne10_block_start*bs*nb10),
-                               (void *)               (wdata + i13*nbw3 + i12*nbw2 + i11*nbw1 + ne10_block_start*nbw0),
-                               (ne10_block_end - ne10_block_start) * bs);
-                }
-            }
-        }
-    #endif
-    }
-	
-    if (ith == 0) {
-        // Every thread starts at ith, so the first unprocessed chunk is nth.  This save a bit of coordination right at the start.
-        atomic_store_explicit(&params->threadpool->current_chunk, nth, memory_order_relaxed);
-    }
-
-    ggml_barrier(params->threadpool);
-
-    // This is the size of the first dimension of the result, so we can iterate that way. (see the ASSERT above, these are the same numbers)
-    const int64_t nr0 = ne0;
-
-    // This is the size of the rest of the dimensions of the result
-    const int64_t nr1 = ne1 * ne2 * ne3;
-
-    // Now select a reasonable chunk size.
-    int chunk_size = 16;
-
-    // We need to step up the size if it's small
-    if (nr0 == 1 || nr1 == 1) {
-        chunk_size = 64;
-    }
-
-    // distribute the work across the inner or outer loop based on which one is larger
-    // The number of chunks in the 0/1 dim.
-    // CEIL(nr0/chunk_size)
-    int64_t nchunk0 = (nr0 + chunk_size - 1) / chunk_size;
-    int64_t nchunk1 = (nr1 + chunk_size - 1) / chunk_size;
-
-    // If the chunking is poor for the number of threads on this setup, scrap the whole plan.  Re-chunk it by thread.
-    //   Also, chunking by thread was measured to have perform better on NUMA systems.  See https://github.com/ggml-org/llama.cpp/pull/6915
-    //   In theory, chunking should be just as useful on NUMA and non NUMA systems, but testing disagreed with that.
-    if (nchunk0 * nchunk1 < nth * 4 || ggml_is_numa()) {
-        // distribute the thread work across the inner or outer loop based on which one is larger
-        nchunk0 = nr0 > nr1 ? nth : 1; // parallelize by src0 rows
-        nchunk1 = nr0 > nr1 ? 1 : nth; // parallelize by src1 rows
-    }
-
-    // The number of elements in each chunk
-    const int64_t dr0 = (nr0 + nchunk0 - 1) / nchunk0;
-    const int64_t dr1 = (nr1 + nchunk1 - 1) / nchunk1;
-	
-    // chunk comes from our thread_id
-    int current_chunk = ith;
-    while (current_chunk < nchunk0 * nchunk1) {
-        const int64_t ith0 = current_chunk % nchunk0;
-        const int64_t ith1 = current_chunk / nchunk0;
-
-        const int64_t ir0_start = dr0 * ith0;
-        const int64_t ir0_end = MIN(ir0_start + dr0, nr0);
-        const int64_t ir1_start = dr1 * ith1;
-        const int64_t ir1_end = MIN(ir1_start + dr1, nr1);
-
-        int64_t num_rows_per_vec_dot = 1; // TB: TODO
-        ggml_compute_forward_mul_mat_one_chunk_IQ4_NL_F32(params, dst, src0->type, num_rows_per_vec_dot, ir0_start, ir0_end, ir1_start, ir1_end);
-
-        if (nth >= nchunk0 * nchunk1) {
-            break;
-        }
-
-        current_chunk = atomic_fetch_add_explicit(&params->threadpool->current_chunk, 1, memory_order_relaxed);
-    }
+	//printf("thread %d computes row %ld to %ld; %ld of %ld\n", ith, from_row, to_row, to_row-from_row+1, num_rows_per_chunk);
+	bpp_IQ4NL_F32_vecmul(src0->data, src1->data, dst->data, vecsize, from_row, to_row);
+	//dummyPrint();
 
 } // ggml_compute_forward_mul_mat_iq4_nl
 
