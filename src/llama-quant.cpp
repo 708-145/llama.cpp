91a95d25ea15276fd36136d5fc7132433e7b21ef
708-145 <tobii@gmx.de>
2025-07-22 22:22:25 +0200

    prep inference

--- src/llama-quant.cpp
+++ src/llama-quant.cpp
@@ -1,3 +1,4 @@
+#include "../ggml/src/ggml-cpu/amx/common.h"
 #include <functional>
 
 #include "llama.h" // Must be first to define LLAMA_API and other core types
@@ -42,7 +43,8 @@ size_t llama_tensor_quantize_smarter_blocks(
 
     const int n_segments = (n_cols + 255) / 256;
 
-    for (int i = 0; i < n_segments; ++i) {
+    parallel_for(n_segments, [&](const int begin, const int end) {
+        for (int i = begin; i < end; ++i) {
         const int64_t col_idx = i * 256;
         const int64_t current_n_cols = std::min((int64_t)256, n_cols - col_idx);
 
@@ -94,7 +96,8 @@ size_t llama_tensor_quantize_smarter_blocks(
 
         // Quantize the segment
         ggml_quantize_chunk(segment_type, segment_src_data.data(), segment_dst_data, 0, n_rows, current_n_cols, segment_imatrix_data);
-    }
+        }
+    });
 
     // Recalculate total_bytes_written after all threads are done.
     size_t total_bytes_written = 0;
